1508990e11315afe664246c35fe8174bfdc8e46e
Sushruth Samson <113278926+sam-0706@users.noreply.github.com>
2025-05-24 14:40:30 +0530

    Update faq_api.py

--- apis/faq_api.py
+++ apis/faq_api.py
@@ -16,3 +16,25 @@ async def handle_faq(req: FAQRequest):
         "answer": response,
         "time_taken_sec": duration
     }
+
+@router.get("/get_jobs")
+async def get_jobs():
+    response= getalljobs()
+    return response
+
+@router.get("/get_job_by_id")
+async def get_job_by_id(job_id: str):
+    response = get_resumes_by_job(job_id)
+    return response
+
+@router.post("/add_job")
+async def add_new_job(request: Request):
+    response =  await request.json()
+    response = add_job(response['job_id'], response['job_description'])
+    return response
+
+@router.post("/add_resume")
+async def add_new_resume(request: Request):
+    response =  await request.json()
+    response = add_resume(response['job_id'], response['resume'], response['name'])
+    return response
